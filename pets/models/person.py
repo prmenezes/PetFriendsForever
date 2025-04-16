ed107d779c82d57444e694d3b77cc9f88a79476b
prmenezes <precilla.menezes@yahoo.co.in>
2025-04-15 20:14:17 -0600

    Chenged phone to charfield

--- pets/models/person.py
+++ pets/models/person.py
@@ -16,7 +16,7 @@ class Person(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     # PV: Phone number = whole number
-    phone = models.IntegerField(help_text="Digits only")
+    phone = models.CharField(max_length=15)
     email = models.EmailField(max_length=254)
     interested_species = models.CharField(choices=species_choices, blank=True, null=True)
     first_time_owner = models.BooleanField(help_text="Please check this box if this will be your first pet")
